031c2404cde4a64bb9d1f6ae039e47f43ec47fd7
liusir-ht <65104142+liusir-ht@users.noreply.github.com>
2026-04-10 20:10:07 +0800

    Update music-card.tsx

--- src/components/music-card.tsx
+++ src/components/music-card.tsx
@@ -12,7 +12,7 @@ import { Pause } from 'lucide-react'
 import { usePathname } from 'next/navigation'
 import clsx from 'clsx'
 
-const MUSIC_FILES = ['/music/Not-Angry.mp3']
+const MUSIC_FILES = ['/music/not-angry.mp3']
 
 export default function MusicCard() {
 	const pathname = usePathname()
